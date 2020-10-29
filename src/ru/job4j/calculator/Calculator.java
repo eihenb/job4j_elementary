e6cfe4bb9cafd2e78c25d1894d7651d58713a5b4
Eduards Eihenbaums <eihenb3333@gmail.com>
2020-10-29 07:57:40 +0000

    4.1. Аргументы [#397649]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,22 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String args[]) {
-        //переменную типа int c именем six и значением 6.
-        //- переменную типа int c именем four и значением 4.
-        //- переменную типа int с именем five и значением 5.
-        int six = 6;
-        int four = 4;
-        int five = 5;
 
-        //2. Создайте еще три переменные для хранения результата арифметических операций.
-        //- переменную типа int c именем sixDivTwo и присвойте ей значение арифметической операции деления переменной six на two.
-        int sixDivTwo = six / 2;
-        //- переменную типа int c именем fiveMinusTwo и присвойте ей значение арифметической операции вычитания переменной five c two.
-        int fiveMinusTwo = five - 2;
-        //- переменную типа int c именем fourTimeTwo и присвойте ей значение арифметической операции умножения переменной four на two.
-        int fourTimeTwo = four * 2;
 
-        //3. Выведите на консоль переменные sixDivTwo, fiveMinusTwo, fourTimeTwo.
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-        //4. Загрузите файл ru.job4j.calculator.Calculator на сайт github.com и оставьте ссылку на изменения.
+        public static void hello(String name) {
+            System.out.println("Hello, " + name);
+        }
+
+        public static void main(String[] args) {
+            String name = "Petr Arsentev";
+            int age = 33;
+
+            Calculator.hello(name);
+
+            Calculator.hello(name);
+
+            Calculator.hello(name);
+
+            Calculator.hello(name);
         }
     }
