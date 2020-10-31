668f02f44ad635d0e9f5eb984c145596a42ac50b
Eduards Eihenbaums <eihenb3333@gmail.com>
2020-10-31 07:31:06 +0000

    4.3.1. Оператор && [#397591]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,19 +1,17 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void main(String[] args) {
-        Cinema.access(15);
-        Cinema.access(20);
-        Cinema.access(65);
+    public static void permission(boolean allowByParents, boolean hasMoney) {
+        if (allowByParents && hasMoney) {
+            System.out.println("I can go to Cinema.");
+        } else {
+            System.out.println("I can't go to Cinema.");
+        }
     }
 
-    public static void access(int age) {
-        System.out.println("The custom age is " + age);
-
-        if (age >= 18) {
-            System.out.println("Access Granted");
-        } else {
-            System.out.println("Sorry next time");
-        }
+    public static void main(String[] args) {
+        Cinema.permission(true, true); // true
+        Cinema.permission(false, true); // false
+        Cinema.permission(true, false); // false
     }
 }
